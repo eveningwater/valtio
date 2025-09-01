29911a613c36033ec6cef3e3dd6f8ad0940751bb
eveningwater.123 <eveningwater.123@bytedance.com>
2025-09-01 17:25:50 +0800

    feat: 调整了文案

--- website/components/layouts/Header/Header.tsx
+++ website/components/layouts/Header/Header.tsx
@@ -98,7 +98,7 @@ export function NavItems() {
           <a className="hover:text-sky-500 dark:hover:text-sky-400">文档</a>
         </Link>
         <Link href="https://valtio.dev/" className='ml-4'>
-          <a className="hover:text-sky-500 dark:hover:text-sky-400">English</a>
+          <a className="hover:text-sky-500 dark:hover:text-sky-400">English文档</a>
         </Link>
       </li>
     </>
